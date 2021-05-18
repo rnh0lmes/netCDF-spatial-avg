dea2a9c3f269934b6055f72553f6b443244b089d
lagc03 <lagc03@gmail.com>
2021-05-18 13:57:31 -0600

    + First public commit.

---
+++ README.md
@@ -0,0 +1,87 @@
+# Downscaled CMIP3 and CMIP5 Climate and Hydrology Projections Pre-processing
+Conversion of targeted netcdf files into spreadsheet tables with the use of the arcpy library for python 2 (arcmap) and 3 (arcgis pro).
+
+## ArcGIS 
++ Desktop arcgis 10.5 or above if using py2 scripts.
++ Desktop arcGIS Pro if using py3 scripts.
+
+## Execution Environment
++ For arcgis desktop you will need python 2.7 which can be setup with anaconda for multiple environments or native 2.7.  
++ For gispro use python 3.6 or above which can be native environment or via anaconda.  
++ Anaconda needs to be the 32 bit version if using python 2 as arcgis desktop is 32 bits.  
+
+To setup anaconda environment with arcmap look at the instructions on:
+
+https://sites.temple.edu/geodev/setting-up-a-python-development-environment-with-anaconda-and-arcpy/
+
+
+Notes: 
++ Download Anaconda 32 bit is using arcmap.  
++ Using arcpy10-7 instead of 10-5
++ Could not setup spyder IDE, but was able to setup the python 2.7 environment with arcpy correctly.
++ The environment created can be used on visual studio code using the python interpreter addon and linking it
+  to the python.exe that is inside the anaconda environment.
+
+To run the python scripts from anaconda:
+
+1. Open de Anaconda prompt.
+2. Activate the environment
+    > activate arcpy10-7
+5. run the script with:
+    > python <script_name>.py
+
+## Input Harvesting
+Refer to the docs folder for instructions on downloading netcdf data.
+TODO: Robyn explain which datasets you downloaded for your inputs.
+
+## Run Instructions
+
+1. Copy the Netcdf files from a single projection into an inputs folder.
+2. Create an outputs folder.
+3. Copy the sub basin shapefile into the shapes folder, make sure to include all files linked to the shapefile.
+4. Modify the path strings that are hardcoded in the python scripts... below all #paths comments. There is one on the main function and one on the cdf processing function.
+5. Modify the level variable to the dimension required.
+
+## Change Log
+
++ Separation of summary table field creation into its own function.
++ Each netcdf file is processed in different cores and independent from each other.
++ Each process created its own local summary table.
++ OutputTable is now managed directly in memory with no storage i/o operations.
++ The main process waits for the other to finish.
++ The main process looks up the tables on the output folder, creates the final summary table and appends the process tables.
++ Final runtime displayed add the end in seconds.
+
+## References
+
++ Downscaled CIMP3 and CMPIP5 Climate and Hydrology Projections, https://gdo-dcp.ucllnl.org/downscaled_cmip_projections/#Projections:%20Subset%20Request
++ Parallel processing on python, https://www.pythonpool.com/python-map-function/
+
+# Questions:
+
+Set environment setting to match pixel size of the raster?
+Setting the environment setting to match the pixel size of the raster being analyzed, makes the tool perform as expected, and as it should be as default, obviously, and therefore matches the output results i get when using zonal histogram on the same exact layers.
+
+What coordinate system is associated with the downscaled projections?
+The downscaled projections are on the NLDAS 1/8th degree grid (datum WGS84).
+
+What coordinate system does the raster use?
+WGS_1984_UTM_Zone_13N
+Geographic Coordinate System:	GCS_WGS_1984
+Datum: 	D_WGS_1984
+
+# Attributions
+
+Original Code: Gerrit VanderWaal (763-647-0338, gerrit.vwaal@gmail.com)
+Project Adaptations:
+ + Robyn Holmes
+ + Luis Garnica
+ + Nolan Townsend
+
+# License
+
+GNU General Public License
+
+
+
+
---
+++ extractor-py3-parallel.py
@@ -0,0 +1,293 @@
+# Purpose: Given a set of netCDF files in a directory, converts the netCDF files to rasters and
+# uses the Zonal Statistics with a supplied zone layer. The created tables are then merged together
+# into a single dBASE table, then converted to an .xls spreadsheet in the chose output directory.
+#
+# Requires: 
+# + Spatial Analyst extension for ArcMap, pre-created input and output directories, and a supplied zone layer.
+# + Python 3
+#
+# MultiProcess: Each cdf file is processed in parallel under separate cores.
+#
+# Original Author: Gerrit VanderWaal (763-647-0338, gerrit.vwaal@gmail.com)
+# Adapted for monthly climate data: Robyn Holmes
+# Adpated for multiprocess: Luis Garnica (lagarnicachavira@utep.edu)
+
+import os
+import datetime
+import arcpy
+import re
+import multiprocessing
+import time
+import gc
+class LicenseError(Exception):
+    pass
+
+def process_cdf(cfd_file):
+    print ("Sub-Processing CDF: " + cfd_file)
+
+    try:
+        # Checks that the Spatial Analyst extension is installed, calls the LicenseError exception if it is not.
+        # print "Checking Spatial Analyst license..."
+        if arcpy.CheckExtension("Spatial") == "Available":
+            arcpy.CheckOutExtension("Spatial")
+            # print "License retrieved.\n"
+        else:
+            raise LicenseError
+
+        # Enables data overwriting.
+        arcpy.env.overwriteOutput = True
+
+        # paths
+        inputSpace = "C:\\Users\\username\\Documents\\Repositories\\netcdf-processor\\inputs"
+        outputSpace = "C:\\Users\\username\\Documents\\Repositories\\netcdf-processor\\outputs"
+        # only the name, no extension for raster files!
+        zones = "C:\\Users\\username\\Documents\\Repositories\\netcdf-processor\\shapes\\Sub_Basins"
+
+        # level dimension
+        level = 0
+
+        # arcpy environment workspace
+        arcpy.env.workspace = inputSpace
+
+        # DAILY new vars
+        dimension = "time"
+        valueSelectionMethod = "BY_VALUE"
+        theFormatTime = '%m/%d/%Y %H:%M:%S %p'
+        theFormat = '%m/%d/%Y'
+
+        # declare local result table
+        result_table = None
+
+        if cfd_file.startswith("Extraction_"):
+            ###DAILY new STEPS
+            # inputs is hardcoded here, need to check use of shares variables
+            # print("input path: "  + inputSpace + "\\" + cfd_file + "\n")
+            nc_FP = arcpy.NetCDFFileProperties(inputSpace + "\\" + cfd_file)
+            nc_Dim = nc_FP.getDimensions()
+
+            # extracts the variable name from the file name
+            fluxvar = cfd_file[11:-3] 
+
+            # for level dimension
+            dimension2 = "projection"   ##THE SECOND DIMENSION##
+            top2 = nc_FP.getDimensionSize(dimension2)   ##THE SECOND DIMENSION##
+
+            # create local summary table for this netcdf file
+            result_table = create_summary_table(outputSpace, "summary_" + fluxvar)
+
+            for dimension in nc_Dim:
+                if dimension == "time":
+                    top = nc_FP.getDimensionSize(dimension)
+                    for i in range(0, top):
+                        dimension_values = nc_FP.getDimensionValue(dimension, i)
+                        thedate = str(dimension_values)
+                        try:
+                            dt = datetime.datetime.strptime(thedate, theFormatTime)
+                        except:
+                            dt = datetime.datetime.strptime(thedate, theFormat)
+                        justdate = str(dt.month) + "/" + str(dt.day) + "/" + str(dt.year)
+                        dv1 = ["time", dimension_values]
+                        # dimension_values = [dv1]
+                        dv2 = ["projection", level]
+                        dimension_values = [dv1,dv2]
+
+                        # Creates a raster from the netCDF file stored in "CDFs" using the "Baseflow" variable, "lon" as the x-dimension, "lat" as the y-dimension, and default variables for
+                        # the rest.
+                        # print "Creating " + fluxvar + " raster for " + justdate + " " + cfd_file + "..."####
+                        rasterFluxvar = arcpy.MakeNetCDFRasterLayer_md (cfd_file, fluxvar, "longitude", "latitude", os.path.join(outputSpace, fluxvar), "", dimension_values, valueSelectionMethod, "CENTER")
+                        
+                        # Creates a Zonal Statistics table using the specified zone layer as the zones, the watershed names as the distinguishing fields, the previously created raster as
+                        # the input raster, and default variables for the rest.
+                        # print "Creating " + fluxvar + " table for " + justdate + " " + cfd_file + "...\n"####
+                        iotype = 'in_memory\\'  
+                        outputTable = iotype + '\\' + fluxvar + "_" + str(dt.month) + "_" + str(dt.day) + "_" + str(dt.year)
+                        arcpy.CreateTable_management(iotype, fluxvar + "_" + str(dt.month) + "_" + str(dt.day) + "_" + str(dt.year), "", "")   
+                        
+                        # print "Calculating statistics \n"
+                        # uses parallel processing by default
+                        arcpy.sa.ZonalStatisticsAsTable (zones, "NAME", rasterFluxvar, outputTable, "", "")
+
+                        # Adds month, year, and type fields to newly created table.
+                        # print "adding fields to stats table"
+                        arcpy.AddField_management(outputTable, "thedate", "TEXT", "", "", 10, "", "", "", "")
+                        arcpy.AddField_management(outputTable, "YEAR", "TEXT", "", "", 4, "", "", "", "")
+                        arcpy.AddField_management(outputTable, "MONTH", "TEXT", "", "", 2, "", "", "", "")
+                        arcpy.AddField_management(outputTable, "DAY", "TEXT", "", "", 2, "", "", "", "")
+                        arcpy.AddField_management(outputTable, "LEVEL", "TEXT", "", "", 3, "", "", "", "")
+                        arcpy.AddField_management(outputTable, "TYPE", "TEXT", "", "", 20, "", "", "", "")                   
+                        
+                        # Populate month, year, and type fields in new table.
+                        # print "adding values to fields"
+                        with arcpy.da.UpdateCursor(outputTable, ["thedate", "YEAR", "MONTH","DAY", "TYPE", "LEVEL"], "", "", "", (None, None)) as cursor:
+                            for row in cursor:
+                                row[0] = justdate####
+                                row[1] = dt.year####
+                                row[2] = dt.month####
+                                row[3] = dt.day####
+                                row[4] = fluxvar####
+                                row[5] = level####
+                                cursor.updateRow(row)
+                        
+                        # Appends newly created table to local result table.
+                        # print "append to list of tables"
+                        arcpy.Append_management(outputTable, result_table, "NO_TEST", "", "")
+                        
+                        # clean up memory use
+                        del outputTable
+                        del rasterFluxvar
+                        gc.collect()
+                        # break #uncomment just to get a small sample first year
+                        # End range for
+                    # End time if
+                 # End dimension for   
+            #End check if it is a netcdf file for extraction  
+        # No need to return anything, tables will be on folder.   
+        return         
+        # End process_cdf
+
+    except Exception as e:
+        # If an error occurred, print line number and error message
+        import traceback, sys
+        tb = sys.exc_info()[2]
+        print ("An error occured on line %i" % tb.tb_lineno)
+        print (str(e))
+        return None
+
+def create_summary_table(outputSpace, summaryFile):
+    # print("Creating summary table")
+    summary = arcpy.CreateTable_management(outputSpace, summaryFile + ".dbf", "", "")
+
+    # Add fields to the table
+    arcpy.AddField_management(summary, "NAME", "TEXT", "", "", 24, "", "", "", "")
+    arcpy.AddField_management(summary, "thedate", "TEXT", "", "", 10, "", "", "", "")
+    arcpy.AddField_management(summary, "YEAR", "TEXT", "", "", 4, "", "", "", "")
+    arcpy.AddField_management(summary, "MONTH", "TEXT", "", "", 2, "", "", "", "")
+    arcpy.AddField_management(summary, "DAY", "TEXT", "", "", 2, "", "", "", "")
+    arcpy.AddField_management(summary, "TYPE", "TEXT", "", "", 20, "", "", "", "")   
+    arcpy.AddField_management(summary, "LEVEL", "LONG", "", "", "", "", "", "", "") 
+    arcpy.AddField_management(summary, "ZONE_CODE", "LONG", "", "", "", "", "", "", "")
+    arcpy.AddField_management(summary, "COUNT", "LONG", "", "", "", "", "", "", "")
+    arcpy.AddField_management(summary, "AREA", "DOUBLE", "", "", "", "", "", "", "")
+    arcpy.AddField_management(summary, "MIN", "DOUBLE", "", "", "", "", "", "", "")
+    arcpy.AddField_management(summary, "MAX", "DOUBLE", "", "", "", "", "", "", "")
+    arcpy.AddField_management(summary, "RANGE", "DOUBLE", "", "", "", "", "", "", "")
+    arcpy.AddField_management(summary, "MEAN", "DOUBLE", "", "", "", "", "", "", "")
+    arcpy.AddField_management(summary, "STD", "DOUBLE", "", "", "", "", "", "", "")
+    arcpy.AddField_management(summary, "SUM", "DOUBLE", "", "", "", "", "", "", "")
+
+    return summary
+
+def main():
+    start_time = time.time()
+    print("Starting Main...")
+    
+    # Checks that the Spatial Analyst extension is installed, calls the LicenseError exception if it is not.
+    print ("Checking Spatial Analyst license...")
+    if arcpy.CheckExtension("Spatial") == "Available":
+        arcpy.CheckOutExtension("Spatial")
+        print ("License retrieved.\n")
+    else:
+        raise LicenseError
+
+    try:
+        # reset environments
+        arcpy.ResetEnvironments()
+
+        environments = arcpy.ListEnvironments()
+
+        # Sort the environment names
+        environments.sort()
+
+        # print environment and setting
+        for environment in environments:
+            # Format and print each environment and its current setting.
+            # (The environments are accessed by key from arcpy.env.)
+            print("{0:<30}: {1}".format(environment, arcpy.env[environment]))
+
+        # Enables data overwriting.
+        arcpy.env.overwriteOutput = True
+
+        # paths
+        inputSpace = "C:\\Users\\username\\Documents\\Repositories\\netcdf-processor\\inputs"
+        outputSpace = "C:\\Users\\username\\Documents\\Repositories\\netcdf-processor\\outputs"
+
+        # arcpy environment workspace
+        arcpy.env.workspace = inputSpace
+        
+        # Stores netCDF files in variable fileList
+        fileList = os.listdir(inputSpace)
+
+        # define how many threads to use? 
+        # the more threads used more ram memory used at once to allocate memory for each one...
+
+        # Sends netCDF files processing to separate cores
+        print("Multi-Processing netCDF Files...")
+        pool = multiprocessing.Pool()
+        pool.map(process_cdf, fileList)
+
+        # close the process pool, will no be used again
+        pool.close()
+
+        # sync with processing jobs (wait for jobs to finish)
+        pool.join()
+
+        # change the arcpy workspace to the output folder
+        arcpy.env.workspace = outputSpace
+
+        # open generated tables from netcdf files
+        tables = arcpy.ListTables()
+
+        # Creates new table that rest of tables will be appended to, with the year as the title
+        summaryFile = "Summary_Extraction"
+        yearTable = create_summary_table(outputSpace, summaryFile)
+
+        # append the generated tables to the final table
+        for table in tables:
+            print("Found: " + table + "\n")
+            arcpy.Append_management(table, yearTable, "NO_TEST", "", "")
+        
+        # Returns Spatial Analyst license for use by others.
+        arcpy.CheckInExtension("Spatial")
+
+        # Converts summary dBASE table to an Excel 2003 table.
+        arcpy.TableToExcel_conversion (yearTable, os.path.join(outputSpace, summaryFile + ".xls"), "", "")
+
+        # Deletes dBASE files generated in constructing Excel file.
+        outputFiles = os.listdir(outputSpace)
+        os.chdir(outputSpace)
+        for toDelete in outputFiles:
+            if toDelete.endswith(".xls"):
+                pass
+            elif toDelete.endswith(".lock"):
+                pass
+            else:
+                os.remove(toDelete)
+            
+        print ("Check " + outputSpace + " for the created summary file, " + summaryFile + ".")
+        
+        # print total run time
+        print("Total Runtime:  %s seconds\n" % (time.time() - start_time))
+
+        # clean arcpy instance    
+        # del arcpy 
+
+        # force garbage collector
+        # gc.collect()
+        
+        # Closes console.
+        input("Script completed. Press Enter to quit.\n")
+        # End main
+
+    # Error handling.
+    except LicenseError:
+        print ("Spatial Analyst license unavailable.")
+        
+    except Exception as e:
+        # If an error occurred, print line number and error message
+        import traceback, sys
+        tb = sys.exc_info()[2]
+        print ("An error occured on line %i" % tb.tb_lineno)
+        print (str(e))
+
+if __name__ == '__main__':
+    main()
